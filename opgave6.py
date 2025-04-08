5d28fd685a63ad778be8157458eb614f336a1f1c
joha7809 <joha7809@gmail.com>
2025-04-08 12:05:15 +0200

    Done opgave6

--- opgave6.py
+++ opgave6.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-#from opgave5 import make_web
+import numpy as np
+from opgave5 import make_web
 
-def visualize_graph(web):
+
+def visualize_graph(web, node_size=700, font_size=10, arrowsize=20):
     
     # Input: netværk som dictionary
     nodes = [key for key in web.keys()]
@@ -10,13 +12,17 @@ def visualize_graph(web):
     print("Nodes:", nodes)
     print("Edges:", edges)
     # Opret en graf
-    G = nx.DiGraph()
+    G = nx.DiGraph() #Intialiserer en rettet graf
+    # Tilføj noder og kanter til grafen
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
-
     # Tegn grafen
-    subax1 = plt.subplot(121)
-    nx.draw(G, with_labels=True, node_color='lightblue', node_size=700, font_size=10, font_weight='bold', arrowsize=20)
+    subax1 = plt.subplot(111)
+    pos = nx.kamada_kawai_layout(G)
+
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=node_size, font_size=font_size, font_weight='bold', arrowsize=arrowsize)
+    #nx.draw(G, with_labels=True, node_color='lightblue', node_size=700, font_size=10, font_weight='bold', arrowsize=20)
+
     plt.title("Netværk visualiseret som graf")
     plt.axis('off')
     # Output: Netværk visualiseret som graf
@@ -24,8 +30,10 @@ def visualize_graph(web):
 
     
 
+if __name__ == "__main__":
+    # Test med et eksempel
+    # Opret et web med 10 noder og maksimalt 4 links pr. node
+    # og minimum 0 links pr. node
+    W = make_web(10, 4)
+    visualize_graph(W)
 
-#Generate a random web with 10 nodes and 3 links per node
-W = {'P1': {'P2', 'P3'}, 'P2': {'P3'}, 'P3': {'tis'}}
-
-visualize_graph(W)
