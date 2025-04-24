b1c7193c341d676d78fd6319bdcf807d857a7a89
Mcwad <marcuswadsackett@gmail.com>
2025-04-24 14:32:31 +0200

    opgave 24 hurtigere fr (sparse)

--- opgave24.py
+++ opgave24.py
@@ -2,7 +2,7 @@ from opgave4 import W1
 from opgave4 import W2
 from opgave17 import modified_link_matrix
 import numpy as np
-from scipy.linalg import null_space
+from scipy.sparse.linalg import eigs
 
 def eigenvector_PageRank(web,d=0.85):
     # Input: web er en ordbog over websider og links.
@@ -16,13 +16,15 @@ def eigenvector_PageRank(web,d=0.85):
     # variabel oprettes, der opbevarer vores modified link matrix
     md = modified_link_matrix(web, list(web),d=d)
 
-    # identitetsmatricen trækkes fra og kernen findes for at finde egenvektoren til egenværdien 1
-    md = md-np.identity(len(web))
-    eigenvector = null_space(md)
+    # egenvektoren for egenværdien 1 findes
+    vals, vecs = eigs(md, k=1, sigma=1.0)
+
+    # elementerne i vektoren bliver reelle og poistive
+    eigenvector = np.abs(vecs[:, 0].real)
 
     # vektoreren normaliseres så summen af elementerne er 1.
     eigenvector /= sum(eigenvector)
-
+    
     #Vi laver et dictionary, der keys er hjemmesider og values er pageranking
-    ranking = {page : rank[0] for page, rank in zip(web, eigenvector)}
+    ranking = {page : rank for page, rank in zip(web, eigenvector)}
     return ranking
