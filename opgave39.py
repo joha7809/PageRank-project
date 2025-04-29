62410d1cff02024abbabcf2783f1133024a087a3
joha7809 <joha7809@gmail.com>
2025-04-29 16:58:27 +0200

    Maybe prettier?

--- opgave39.py
+++ opgave39.py
@@ -5,11 +5,9 @@ import networkx as nx
 from opgave24 import eigenvector_PageRank
 from opgave5 import make_web
 
+def plot_ranking(web, ranking, d=0.85):
 
-
-
-def plot_ranking(web, ranking, d=0.85, node_size = 7500):
-
+    node_size = 500*len(web.keys())
     
     page_rank = [rank * node_size for rank in list(ranking.values())]
     nodes = [key for key in web.keys()]
@@ -22,10 +20,13 @@ def plot_ranking(web, ranking, d=0.85, node_size = 7500):
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
     # Tegn grafen
+    plt.figure(figsize=(14, 10))  # Before plt.subplot
     subax1 = plt.subplot(111)
     pos = nx.kamada_kawai_layout(G)
+    #pos = nx.spring_layout(G, k=1.5, seed=42)  # Tweak 'k' for spacing
 
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=page_rank, font_size=10, font_weight='bold')
+
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=page_rank, font_size=10, font_weight='bold')
     #nx.draw(G, with_labels=True, node_color='lightblue', node_size=700, font_size=10, font_weight='bold', arrowsize=20)
 
     plt.title("Netværk visualiseret som graf")
@@ -33,5 +34,5 @@ def plot_ranking(web, ranking, d=0.85, node_size = 7500):
     # Output: Netværk visualiseret som graf
     plt.show()
 
-web = make_web(6,4)
+web = make_web(100,5)
 plot_ranking(web,eigenvector_PageRank(web,0.85),0.85)
