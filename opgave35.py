2015efc8eef955b61a2701c8686fcd1c16d59336
AJungersen <70943604+AJungersen@users.noreply.github.com>
2025-04-29 09:34:57 +0200

    oprettet opg 35

---
+++ opgave35.py
@@ -0,0 +1,13 @@
+def matrix_PageRank(web,power,d=0.85):
+
+    # Input: web er et dictionary med websider og links.
+    # d er en positiv float, dæmpningskonstanten.
+    # Output: Et dictionary med de samme nøgler som web, og værdierne er PageRank for hver nøgle.
+
+    ranking = dict()
+
+    # INDSÆT KODE HER
+
+    
+
+    return ranking
