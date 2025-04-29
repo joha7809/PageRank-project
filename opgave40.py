7ac734b48dc6d10bab94fb8d722c7c5f873d55d5
Jodsper <j.norborg@live.dk>
2025-04-29 11:52:20 +0200

    Create opgave40.py

---
+++ opgave40.py
@@ -0,0 +1,8 @@
+from opgave5 import make_web
+from opgave12 import random_surf_damp
+from opgave15 import recursive_PageRank
+from opgave24 import eigenvector_PageRank
+from opgave35 import matrix_PageRank
+
+web = make_web(50,29)
+
