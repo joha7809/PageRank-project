5ec96117b2f5db2100ab94f54f4f66add27a751e
Mcwad <marcuswadsackett@gmail.com>
2025-04-30 20:40:33 +0200

    :c

--- opgave35.py
+++ opgave35.py
@@ -2,26 +2,14 @@ import numpy as np
 from opgave17 import modified_link_matrix
 from opgave4 import W1, W2
 
-
-def matrix_PageRank(web, power, d=0.85, stop_value=0):
+def matrix_PageRank(web, power, d=0.85):
 
     # Input: web er et dictionary med websider og links.
     # d er en positiv float, dæmpningskonstanten.
     # Output: Et dictionary med de samme nøgler som web, og værdierne er PageRank for hver nøgle.
 
     md = modified_link_matrix(web, list(web), d=d)
-    # original md som vi ganger med i loopet
-    original_md = md.copy()
-    # forrige md så vi kan sammenligne ændringen
-    previous_md = md.copy()
-    i = 0
-    for i in range(1, power):
-        md = md @ original_md
-        # finder den største forskel mellem md og previous_md
-        if max(map(abs, md[:,0]-previous_md[:, 0])) < stop_value: break
+    md = np.linalg.matrix_power(md, power)
+    ranking = {page : rank for page, rank in zip(web, md[:, 0])}
 
-        previous_md = md.copy()
-
-    ranking = {page : rank for page, rank in zip(web, md[:,0])}
-
-    return ranking, i+1
+    return ranking
--- opgave40.py
+++ opgave40.py
@@ -3,6 +3,39 @@ from opgave12 import random_surf_damp
 from opgave15 import recursive_PageRank
 from opgave24 import eigenvector_PageRank
 from opgave35 import matrix_PageRank
+import time
 
-web = make_web(50,29)
+web = make_web(5000,100)
+averages = [0]*4
+times = [0]*4
+d = 0.85
 
+times[2] = time.time()
+eigen = eigenvector_PageRank(web, d=d)
+times[2] = time.time()-times[2]
+
+"""
+times[0] = time.time()
+surf, it1 = random_surf_damp(web, 10000000, d=d, stop_value=1e-5)
+times[0] = time.time()-times[0]
+for page1,page2 in zip(eigen.values(),surf.values()):
+    averages[0] += abs(page1-page2)
+#"""
+
+times[1] = time.time()
+rec, it2 = recursive_PageRank(web, d=d, stop_value=1e-12, max_iterations=1000)
+times[1] = time.time()-times[1]
+for page1,page2 in zip(eigen.values(),rec.values()):
+    averages[1] += abs(page1-page2)
+
+times[3] = time.time()
+power = matrix_PageRank(web, power=32, d=d)
+times[3] = time.time()-times[3]
+for page1,page2 in zip(eigen.values(),power.values()):
+    averages[3] += abs(page1-page2)
+
+for i in range(len(averages)):
+    averages[i] /= len(eigen)
+
+print(averages)
+print(times)
